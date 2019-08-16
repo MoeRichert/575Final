ba4056b508a40dc8730f7ae9337b0f32e5f8c44b
shbartlett <48871675+shbartlett@users.noreply.github.com>
2019-08-15 21:29:28 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,12 +13,9 @@ function mymap() {
 var ustates = 
 var seStates = 
 var counties = 
-var basemap = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 0,
-	maxZoom: 20,
-	ext: 'png'
+var basemap = ('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 16
 	
 };
 
