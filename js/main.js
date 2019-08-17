282cb0acd1673069a3860ac4c935df4fd2e77846
shbartlett <shbartlett@users.noreply.github.com>
2019-08-17 19:40:22 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,9 +22,6 @@ var map = d3.select("body")
     .attr("width", width)
     .attr("height", height);
 
-var projection = d3.geoMercator()
-    .scale(300)
-    .translate([width / 3, height / 2]);
 
 //scales
 var x = d3.scaleLinear()
