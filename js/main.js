fb30b884fe8629b346e3f41933e2e15098c91727
shbartlett <shbartlett@users.noreply.github.com>
2019-08-17 14:06:41 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -60,9 +60,7 @@ function callback(error, csvData, usStates, selectStates, rawCounties){
     var states = new L.GeoJSON.AJAX(procallStates, {style: statesStyle});
     var seStates = new L.GeoJSON.AJAX(proselectStates, {style: seStyle});
     var counties = new L.GeoJSON.AJAX(procCounties, {style: countyStyle}).bringToBack();
-    var basemap = ('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-	maxZoom: 16;
+
 
     createMap(map, states, seStates, counties, csvData, CountiesData);
     createCharts();
