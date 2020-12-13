d95300c5f02bbd1eff50f9cd466076d0eb88eba1
MoeRichert <46945488+MoeRichert@users.noreply.github.com>
2020-12-13 15:41:34 -0600

    fix map display

--- js/main.js
+++ js/main.js
@@ -12,6 +12,10 @@ outdoors = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }),
+    dark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+}),
 empty = L.tileLayer('', {
     maxZoom: 20,
     attribution: 'Map created by: Moe R, Stephanie B, and Dwight F'
